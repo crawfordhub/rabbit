3232874a3f4fc7bcb146f28dd4f32364e9251ea9
Ehsan Zahedinejad <ehsanzahedinejad@gmail.com>
2017-03-30 01:08:55 -0700

    Add files via upload
    Debugged the code-- loop works- make sure to use it on resized data and compare the result with not-cropped images to see what is the effect of cropping here

--- remove_background_fnc.py
+++ remove_background_fnc.py
@@ -1,24 +1,25 @@
 import cv2
 import numpy as np
 import os
+import errno
 
 #image has to be resized using the other code
 # define the upper and lower boundaries of the HSV pixel
 # intensities to be considered 'skin'
 
 #this is the input file This could be in a loop
-def img_to_skin(img):
-    img = cv2.imread(img)
+def img_to_skin(fileinput,output_dir,inp):
+    img = cv2.imread(fileinput)
     converted = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
     # lower mask (0-10)
-    lower_red = np.array([0,13,100])
-    upper_red = np.array([10,225,240])
+    lower_red = np.array([0,25,75])
+    upper_red = np.array([10,225,255])
     mask0 = cv2.inRange(converted, lower_red, upper_red)
-
+    
     # upper mask (170-180)
-    lower_red = np.array([135,13,100])
-    upper_red = np.array([180,225,240])
+    lower_red = np.array([120,25,75])
+    upper_red = np.array([180,225,255])
     mask1 = cv2.inRange(converted, lower_red, upper_red)
 
     # join my masks
@@ -29,24 +30,30 @@ def img_to_skin(img):
     skinMask = cv2.GaussianBlur(skinMask, (5, 5), 0)
     skin = cv2.bitwise_and(img, img, mask = skinMask)
     #this is the outputfile
-    cv2.imwrite('output'+img+'.jpg',skin)
-
+    print output_dir+inp
+    cv2.imwrite(output_dir+inp,skin)
     return skin
 
 
-data_dir = "../sagebrush/kaggle/Intel_Data/train/"
+data_dir = "/Users/ezahedin/Downloads/kaggle/train/"
+outpath="/Users/ezahedin/Desktop/Kaggle_project/outputs/"
+
+
+
 type_folders = []
 for filename in os.listdir(data_dir):
     ''' this lists:
-    type 1
-    type 2
-    type 3
+    type_1
+    type_2
+    type_3
     '''
     type_folders.append(filename)
 
 for type_f in type_folders:
+    if not os.path.exists(outpath+type_f):
+        os.mkdir(outpath+type_f)
     if 'DS' not in type_f:
         for image_file in os.listdir(data_dir+type_f):
             if 'DS' not in image_file:
-                img_to_skin(data_dir+type_f+image_file)
+                img_to_skin(data_dir+type_f+'/'+image_file,outpath,type_f+'/'+image_file)
 
