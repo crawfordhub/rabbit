3039fb8b77965764cf851dc0d4bb292fc4287959
JasOberoi <jaspreet.s.oberoi@gmail.com>
2017-04-04 10:32:58 -0700

    Update bounding_box.py
    Edited a bit.

--- bounding_box.py
+++ bounding_box.py
@@ -4,19 +4,11 @@ Created on Mar 30, 2017
 @author: Jas
 '''
 
-import numpy as np
-import matplotlib.pyplot as plt
-from scipy import ndimage as ndi
-
-import PIL
 from PIL import Image
 
 imagePath = "1013.jpg"
 
 im = Image.open(imagePath)
-x = im.split()
-print x
-r,g,b = im.split()
 left,upper,right,lower = im.getbbox()
 tempImage = im.crop((left,upper,right,lower))
 newPath = imagePath.split(".")[0]+"_Cropped" + "." + imagePath.split(".")[1]
